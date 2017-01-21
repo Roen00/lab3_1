4ebb10a4aa5b04f53469f5ea32667ffd5a9d72e9
Rafal Zelek <r.zelek@oberthur.com>
2017-01-21 17:17:07 +0100

    added template for the testCase1

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,7 +1,22 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BookKeeperTest {
 
+    @Test
+    public void testCase1(){
+        //Given
+        final InvoiceFactory invoiceFactory = new InvoiceFactory();
+        final BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+        
+        //When
+
+
+        //Then
+
+
+    }
 }
